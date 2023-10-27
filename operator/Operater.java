6dc14dbb3a50f36e077aca05a90f5fdf61736aee
SaurabhKori <147341400+SaurabhKori@users.noreply.github.com>
2023-10-27 23:17:03 +0530

    Add files via upload

---
+++ operator/Operater.java
@@ -0,0 +1,16 @@
+class Operater{
+       
+public static void main(String[] args)
+{
+int a=40;
+int b=20;
+int sum=a+b;
+int mult=a*b;
+int div=a/b;
+int reminder=a%b;
+System.out.println(sum);
+System.out.println(mult);
+System.out.println(div);
+System.out.println(reminder);
+}
+}
