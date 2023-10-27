969404a4a2679bcc69a8e6a4c9fd94fd62cb93b1
SaurabhKori <147341400+SaurabhKori@users.noreply.github.com>
2023-10-27 23:22:36 +0530

    Add files via upload

---
+++ Controlflow/even100.java
@@ -0,0 +1,16 @@
+
+class even100
+{  
+   public static void main(String[] args)
+      {
+       int a=1;
+      while(a<=100)
+      {
+       if(a%2==0)
+       {
+        System.out.println("even : "+a);
+        }
+       a++;
+       }
+       }
+       }
